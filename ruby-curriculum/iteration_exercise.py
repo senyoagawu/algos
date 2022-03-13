2012a0c9fbb9a13227b7adfcdeb0d688c126d286
Senyo Agawu <senyo.k.agawu@gmail.com>
2022-03-13 13:41:20 -0400

    convert to python

--- ruby-curriculum/iteration_exercise.py
+++ ruby-curriculum/iteration_exercise.py
@@ -19,7 +19,6 @@ def isPrime(n):
     return True
     
 def prime_factorization(n):
-    i = 2
     factors =[]
 
 
@@ -84,100 +83,87 @@ print(prime_factorization(2384912))
 #
 # http://stackoverflow.com/questions/827649/what-is-the-ruby-spaceship-operator
 
-# class Array
-#   def bubble_sort!(&prc)
-#   end
+class Array:
+  def bubble_sort(prc):
+    pass
 
-#   def bubble_sort(&prc)
-#   end
-# end
+  def bubble_sort(prc):
+    pass
+# ### Substrings and Subwords
+#
+# Write a method, `substrings`, that will take a `String` and return an
+# array containing each of its substrings. Don't repeat substrings.
+# Example output: `substrings("cat") => ["c", "ca", "cat", "a", "at",
+# "t"]`.
+#
+# Your `substrings` method returns many strings that are not true English
+# words. Let's write a new method, `subwords`, which will call
+# `substrings`, filtering it to return only valid words. To do this,
+# `subwords` will accept both a string and a dictionary (an array of
+# words).
 
-# # ### Substrings and Subwords
-# #
-# # Write a method, `substrings`, that will take a `String` and return an
-# # array containing each of its substrings. Don't repeat substrings.
-# # Example output: `substrings("cat") => ["c", "ca", "cat", "a", "at",
-# # "t"]`.
-# #
-# # Your `substrings` method returns many strings that are not true English
-# # words. Let's write a new method, `subwords`, which will call
-# # `substrings`, filtering it to return only valid words. To do this,
-# # `subwords` will accept both a string and a dictionary (an array of
-# # words).
+def substrings(string):
+    pass
+def subwords(word, dictionary):
+    pass
+# ### Doubler
+# Write a `doubler` method that takes an array of integers and returns an
+# array with the original elements multiplied by two.
 
-# def substrings(string)
-# end
+def doubler(array):
+    pass
+# ### My Each
+# Extend the Array class to include a method named `my_each` that takes a
+# block, calls the block on every element of the array, and then returns
+# the original array. Do not use Enumerable's `each` method. I want to be
+# able to write:
+#
+# ```ruby
+# # calls my_each twice on the array, printing all the numbers twice.
+# return_value = [1, 2, 3].my_each do |num|
+#   puts num
+#   puts num:
+# # => 1:
+#      2
+#      3
+#      1
+#      2
+#      3
+#
+# p return_value # => [1, 2, 3]
+# ```
 
-# def subwords(word, dictionary)
-# end
+class Array:
+  def my_each(prc):
+    pass
+# ### My Enumerable Methods
+# * Implement new `Array` methods `my_map` and `my_select`. Do
+#   it by monkey-patching the `Array` class. Don't use any of the
+#   original versions when writing these. Use your `my_each` method to
+#   define the others. Remember that `each`/`map`/`select` do not modify
+#   the original array.
+# * Implement a `my_inject` method. Your version shouldn't take an
+#   optional starting argument; just use the first element. Ruby's
+#   `inject` is fancy (you can write `[1, 2, 3].inject(:+)` to shorten
+#   up `[1, 2, 3].inject { |sum, num| sum + num }`), but do the block
+#   (and not the symbol) version. Again, use your `my_each` to define
+#   `my_inject`. Again, do not modify the original array.
 
-# # ### Doubler
-# # Write a `doubler` method that takes an array of integers and returns an
-# # array with the original elements multiplied by two.
+class Array:
+  def my_map(prc):
+    pass
+  def my_select(prc):
+    pass
+  def my_inject(blk):
+    pass
+# ### Concatenate
+# Create a method that takes in an `Array` of `String`s and uses `inject`
+# to return the concatenation of the strings.
+#
+# ```ruby
+# concatenate(["Yay ", "for ", "strings!"])
+# # => "Yay for strings!"
+# ```
 
-# def doubler(array)
-# end
-
-# # ### My Each
-# # Extend the Array class to include a method named `my_each` that takes a
-# # block, calls the block on every element of the array, and then returns
-# # the original array. Do not use Enumerable's `each` method. I want to be
-# # able to write:
-# #
-# # ```ruby
-# # # calls my_each twice on the array, printing all the numbers twice.
-# # return_value = [1, 2, 3].my_each do |num|
-# #   puts num
-# # end.my_each do |num|
-# #   puts num
-# # end
-# # # => 1
-# #      2
-# #      3
-# #      1
-# #      2
-# #      3
-# #
-# # p return_value # => [1, 2, 3]
-# # ```
-
-# class Array
-#   def my_each(&prc)
-#   end
-# end
-
-# # ### My Enumerable Methods
-# # * Implement new `Array` methods `my_map` and `my_select`. Do
-# #   it by monkey-patching the `Array` class. Don't use any of the
-# #   original versions when writing these. Use your `my_each` method to
-# #   define the others. Remember that `each`/`map`/`select` do not modify
-# #   the original array.
-# # * Implement a `my_inject` method. Your version shouldn't take an
-# #   optional starting argument; just use the first element. Ruby's
-# #   `inject` is fancy (you can write `[1, 2, 3].inject(:+)` to shorten
-# #   up `[1, 2, 3].inject { |sum, num| sum + num }`), but do the block
-# #   (and not the symbol) version. Again, use your `my_each` to define
-# #   `my_inject`. Again, do not modify the original array.
-
-# class Array
-#   def my_map(&prc)
-#   end
-
-#   def my_select(&prc)
-#   end
-
-#   def my_inject(&blk)
-#   end
-# end
-
-# # ### Concatenate
-# # Create a method that takes in an `Array` of `String`s and uses `inject`
-# # to return the concatenation of the strings.
-# #
-# # ```ruby
-# # concatenate(["Yay ", "for ", "strings!"])
-# # # => "Yay for strings!"
-# # ```
-
-# def concatenate(strings)
-# end
+def concatenate(strings):
+    pass
