15cc04bbceca6260c20f215b1ba94bc0cecac9a3
Senyo Agawu <senyo.k.agawu@gmail.com>
2022-03-12 19:08:30 -0500

    and this

--- virtual-contests/283/lc-2195.py
+++ virtual-contests/283/lc-2195.py
@@ -2,20 +2,24 @@ def minimalKSum(nums, k):
     ans = []
     n = 1
     pointer = 0
-    while len(ans) < k: 
-        while pointer < len(nums):
-            num = nums[pointer]
-            if n < num:
-                ans.append(n)
-            else:
-                pointer += 1
-            n += 1
-        if pointer >= len(nums):
-            ans.append(n)   
-            n+=1
+    while len(ans) < k and pointer < len(nums):
+        print(ans) 
+        num = nums[pointer]
+        if n < num:
+            ans.append(n)
+        else:
+            pointer += 1
+        n += 1
+    if pointer > len(nums): #out of range
+        ans.append(n)   
+        n+=1
+    for _ in range(k - len(nums)-2):
+        print(ans) 
+        ans.append(n)
+        n += 1
     return sum(ans)    
 
 
  
-print(minimalKSum([1,4,25,10,25], 2))
-# print(minimalKSum([5,6], 6))
+# print(minimalKSum([1,4,25,10,25], 2))
+print(minimalKSum([5,6], 6))
