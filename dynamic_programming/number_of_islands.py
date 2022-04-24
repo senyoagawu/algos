ce955a91b94d6291e1bbc9a675700f4a7f9b2db2
Senyo Agawu <senyo.k.agawu@gmail.com>
2022-04-23 22:26:58 -0400

    cleanup

---
+++ dynamic_programming/number_of_islands.py
@@ -0,0 +1,4 @@
+(col+1, val)
+            (col-1, val)
+            (col, val +1)
+            (col, val -1)
--- subsets_permutations.js
+++ subsets_permutations.js
@@ -15,12 +15,12 @@ const dfs = node => {
 
 }
 
-function subsets(arr, len=null) {
+function subsets(arr, len = null) {
   // base case 
   if (arr.length === 0) return [[]];
   let val;
-  while (true){
-    [val, arr] =  [arr[0], arr.slice(1)]
+  while (true) {
+    [val, arr] = [arr[0], arr.slice(1)]
     new_subsets = arr.map(sub => sub)
   }
   const last = arr[arr.length - 1];
@@ -31,5 +31,14 @@ function subsets(arr, len=null) {
 
 }
 
+const combinations = (set, set_size) => {
+  if (!set.length) return [[]];
+  const [first, subsets] = [set[0], combinations(set.slice(1))]
+  console.log(subsets)
+  new_subsets = [first, ...subsets]
+  console.log(new_subsets)
+}
 
-subsets([1, 2, 3, 4, 5])
+
+combinations([1, 2, 3, 4, 5])
+
